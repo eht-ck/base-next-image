2a1acb1566f3eaba1577536a4a666a4ed139ce1d
Chandresh Kumar <chandresh.beas@gmail.com>
2025-02-08 11:44:48 +0530

    feat: initial commit

---
+++ src/app/components/LandingPage.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return <div> SPEEDY PAGES DEFAULT </div>;
+};
+
+export default LandingPage;
---
+++ src/app/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100"></div>
+  );
+};
+
+export default Home;
